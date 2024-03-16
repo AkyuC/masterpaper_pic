845d60b3550b08a3970b8965203d3811e45913a0
AkyuC <937818146@qq.com>
2024-03-16 16:14:55 +0800

    .

--- bc/script/GAE.py
+++ bc/script/GAE.py
@@ -8,17 +8,21 @@ plt.rcParams['font.size'] = 15
 plt.figure(figsize=(10,3))
 
 avg_finished_time = np.loadtxt("avg_finished_time7.0")
+print(min(avg_finished_time))
 avg_finished_time_without_GAE = np.loadtxt("avg_finished_time7.1")
+print(min(avg_finished_time_without_GAE))
+avg_finished_time_without_scaling = np.loadtxt("avg_finished_time7.6")
 
 datalen = 2010 if len(avg_finished_time) > 2010 else len(avg_finished_time)
 
 plt.plot(np.arange(datalen), avg_finished_time[:datalen], label="Ours", linewidth = 2)
 plt.plot(np.arange(datalen), avg_finished_time_without_GAE[:datalen], label="Ours Without Duration", linewidth = 2, alpha = 0.75)
+plt.plot(np.arange(datalen), avg_finished_time_without_scaling[:datalen], label="Ours Without Scaling", linewidth = 2, alpha = 0.75)
 
 
 plt.ylabel("完成时间 / 秒")
 plt.xlabel("训练回合")
-plt.yticks([300,400,500,600,700])
+# plt.yticks([300,400,500,600,700])
 plt.legend()
 plt.savefig("GAE_compared.png", bbox_inches='tight', pad_inches=0.05, dpi=600)
 plt.close('all')
