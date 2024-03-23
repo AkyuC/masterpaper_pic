3029729c6b66e6bfa1c0048ed24e6e784ed144a2
AkyuC <937818146@qq.com>
2024-03-23 18:03:38 +0800

    .

--- bc/script/metric_partition.py
+++ bc/script/metric_partition.py
@@ -72,6 +72,31 @@ plt.legend()
 plt.savefig("avg_finished_time7.3.png", bbox_inches='tight', pad_inches=0.05, dpi=600)
 plt.close('all')
 
+plt.close('all')
+
+
+plt.rcParams['font.sans-serif'] = 'SimSun'
+plt.rcParams['font.serif'] = 'Times New Roman'
+plt.rcParams['font.size'] = 15
+
+plt.figure(figsize=(10,3))
+
+avg_finished_time = np.loadtxt("avg_finished_time7.9")
+
+datalen = 2510 if len(avg_finished_time) > 2510 else len(avg_finished_time)
+
+plt.plot(np.arange(datalen), avg_finished_time[:datalen], label="Ours", linewidth = 2)
+plt.plot(np.arange(datalen), np.ones(datalen)*1514.90, label="Optimal", alpha = 0.75, linewidth = 2)
+plt.plot(np.arange(datalen), np.ones(datalen)*2422.84, label="Nearest", alpha = 0.75, linewidth = 2)
+plt.plot(np.arange(datalen), np.ones(datalen)*2122.66, label="Partition Nearest", alpha = 0.75, linewidth = 2)
+plt.plot(np.arange(datalen), np.ones(datalen)*5900.95, label="Partition Random", alpha = 0.75, linewidth = 2)
+
+plt.ylabel("完成时间 / 秒")
+plt.xlabel("训练回合")
+# plt.yticks([300,400,500,600,700])
+plt.legend()
+plt.savefig("avg_finished_time7.9.png", bbox_inches='tight', pad_inches=0.05, dpi=600)
+plt.close('all')
 
 
 plt.rcParams['font.sans-serif'] = 'SimSun'
